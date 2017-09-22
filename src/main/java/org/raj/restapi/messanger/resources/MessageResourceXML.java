03646ad7eaf78a05a992ae96e4da52ab2a8641fb
Sunny Singh <ssingh@netlinkvoice.com>
2017-09-22 18:54:10 -0500

    in memory data

--- src/main/java/org/raj/restapi/messanger/resources/MessageResourceXML.java
+++ src/main/java/org/raj/restapi/messanger/resources/MessageResourceXML.java
@@ -6,27 +6,20 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.PathParam;
 
 import org.raj.restapi.messanger.service.MessageService;
 import org.raj.restapi.messanger.model.Message;
 
-@Path("/messages")
-public class MessageResource {
+@Path("/messages-xml")
+public class MessageResourceXML {
 	
 	MessageService messageService = new MessageService();
 	
 	@GET
 	@Produces(MediaType.APPLICATION_XML)
 	public List<Message> getMessages(){
-		return messageService.getAllMessages();
+		return messageService.getAllMessagesXML();
 	}
 	
-	@GET
-	@Path("/{messageId}")
-	@Produces(MediaType.APPLICATION_XML)
-	public Message getMessage(@PathParam("messageId") long id){
-		return  messageService.getMessage(id);
-	}
 	
 }
--- src/main/java/org/raj/restapi/messanger/service/MessageService.java
+++ src/main/java/org/raj/restapi/messanger/service/MessageService.java
@@ -2,12 +2,16 @@ package org.raj.restapi.messanger.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+
+
+import org.raj.restapi.messanger.database.DatabaseClass;
 import org.raj.restapi.messanger.model.Message;
 
 public class MessageService {
 	
-	public List<Message> getAllMessages(){
+	public List<Message> getAllMessagesXML(){
 		Message m1 = new Message(1L, "First Message Hello ", "Shailesh Singh");
 		Message m2 = new Message(2L, "Second Message Hello Jersey", "Shailesh Singh");
 		
@@ -18,5 +22,42 @@ public class MessageService {
 		
 		return m_list;
 				
+	}
+	
+	private Map<Long, Message> messages = DatabaseClass.getMessages();
+	
+	
+	public MessageService() {
+		messages.put(1L, new Message(1,"hello world", "Shailesh Singh"));
+		messages.put(2L, new Message(2,"hello world second jersey msg", "Shailesh Singh"));
+		
+	}
+	
+	
+	
+	public List<Message> getAllMessages(){
+		return new ArrayList<Message>(messages.values());
+	}
+	
+	public Message getMessage(long id) {
+		return messages.get(id);
+	}
+	
+	public Message addMessage(Message message) {
+		message.setId(messages.size()+1);
+		messages.put(message.getId(), message);
+		return message;
+	}
+	
+	public Message UpdateMessage(Message message) {
+		if(message.getId()<=0) {
+			return null;
+		}
+		messages.put(message.getId(), message);
+		return message;
+	}
+	
+	public Message removeMessage(long id) {
+		return messages.remove(id);
 	}
 }
